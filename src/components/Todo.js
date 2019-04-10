029b32e14cf793159a026324e266bd6a0494ba2a
New-Phyrexia <Dylanss992@gmail.com>
2019-04-09 22:03:44 -0600

    This is Dylans code

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import RemoveTodo from '../containers/RemoveTodo';
 
-const Todo = ({ onClick, completed, text }) => (
-  <li
+const Todo = ({ onClick, completed, text, id }) => (
+  <li>
+    <div align="center"
     onClick={onClick}
     style={{
       textDecoration: completed ? 'line-through' : 'none'
     }}
   >
     {text}
+    </div><div><RemoveTodo id={id}/></div>
   </li>
 )
 
 Todo.propTypes = {
   onClick: PropTypes.func.isRequired,
   completed: PropTypes.bool.isRequired,
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
 }
 
 export default Todo
